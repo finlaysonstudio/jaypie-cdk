1e600db53ed2a1a6d355e06f52d047c6c43d9053
Adam Finlayson <amf@adamfinlayson.com>
2024-04-03 19:34:59 -0400

    chore: config: jest matcher

---
+++ jaypie.jest.js
@@ -0,0 +1,17 @@
+global.jest.expect.extend({
+  isJaypieError(received) {
+    const pass = received instanceof Error && received.isProjectError === true;
+
+    if (pass) {
+      return {
+        message: () => `expected ${received} not to be a Jaypie Error`,
+        pass: true,
+      };
+    } else {
+      return {
+        message: () => `expected ${received} to be a Jaypie Error`,
+        pass: false,
+      };
+    }
+  },
+});
