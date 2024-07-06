db19ccb33811eed73f5e11bed7a81fc818b1ce83
Adam Finlayson <amf@adamfinlayson.com>
2024-07-06 13:01:11 -0500

    chore: install: config: jaypie eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,5 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true,
-  },
-  extends: [
-    "eslint:recommended",
-    "plugin:import/recommended",
-    // "Add plugin:prettier/recommended as the last item in the extends array in your .eslintrc* config file, so that eslint-config-prettier has the opportunity to override other configs"
-    "plugin:prettier/recommended",
-  ],
+  extends: ["jaypie/commonjs", "plugin:jest/recommended"],
   overrides: [
     {
       files: ["__tests__/**", "**/*.spec.js", "**/*.test.js"],
@@ -20,21 +11,4 @@ module.exports = {
       },
     },
   ],
-  parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module",
-  },
-  plugins: ["prettier"],
-  rules: {
-    "import/extensions": ["error", "ignorePackages"],
-    "import/no-unresolved": ["error", { commonjs: true, caseSensitive: true }],
-    "no-console": "warn",
-    "no-restricted-syntax": [
-      "error",
-      "ExportNamedDeclaration",
-      "ExportDefaultDeclaration",
-      "ExportAllDeclaration",
-      "ImportDeclaration",
-    ],
-  },
 };
